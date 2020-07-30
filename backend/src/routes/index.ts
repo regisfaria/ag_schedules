5ad018460800edcf8c8fdd44c59e9593cf7b800d
regisfaria <rgisfaria@yahoo.com.br>
2020-07-29 21:12:00 -0300

    added new routes in the index router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,11 +3,15 @@ import { Router } from 'express';
 import usersRouter from './users.routes';
 import sessionsRouter from './sessions.routes';
 import profilesRouter from './profiles.routes';
+import pacientsRouter from './pacients.routes';
+import consultsRouter from './consults.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/profiles', profilesRouter);
+routes.use('/pacients', pacientsRouter);
+routes.use('/consults', consultsRouter);
 
 export default routes;
