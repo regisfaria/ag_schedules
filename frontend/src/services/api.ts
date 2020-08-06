896bffae509d42724f87e31d065b9fde16a2240c
regisfaria <rgisfaria@yahoo.com.br>
2020-08-05 22:56:06 -0300

    added token into the api requests

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,20 @@
 import axios from 'axios';
 
+function getToken() {
+  const token = localStorage.getItem('@AGSchedules:token');
+
+  if (token) {
+    return token;
+  }
+
+  return null;
+}
+
 const api = axios.create({
   baseURL: 'http://localhost:3333/',
+  headers: {
+    Authorization: `Bearer ${getToken()}`,
+  },
 });
 
 export default api;
