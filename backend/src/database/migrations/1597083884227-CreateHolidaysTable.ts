f5d1bb1642d295b939f94c3d271f9aa61b4d4a60
regisfaria <rgisfaria@yahoo.com.br>
2020-08-10 17:52:48 -0300

    added a new migration to the database

---
+++ backend/src/database/migrations/1597083884227-CreateHolidaysTable.ts
@@ -0,0 +1,62 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateHolidaysTable1597083884227
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'holidays',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'scheduleAvailabilityId',
+            type: 'uuid',
+          },
+          {
+            name: 'day',
+            type: 'date',
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'holidays',
+      new TableForeignKey({
+        name: 'ScheduledHolidays',
+        columnNames: ['scheduleAvailabilityId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'schedules_availability',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('holidays', 'ScheduledHolidays');
+
+    await queryRunner.dropTable('holidays');
+  }
+}
