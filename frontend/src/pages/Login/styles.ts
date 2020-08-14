85822821e194502cb783675d2c29ad005352d4a4
akioishiguro <akiosantos@gmail.com>
2020-08-14 13:45:47 -0300

    Styles Login Page

--- frontend/src/pages/Login/styles.ts
+++ frontend/src/pages/Login/styles.ts
@@ -1,31 +1,56 @@
 import styled from 'styled-components';
-import logoImg from '../../assets/logo.png';
 
 export const LoginWindow = styled.div`
-  width: 40rem;
-  height: 34rem;
-  border-radius: 40px;
-
   display: flex;
-  align-items: stretch;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
-  margin: auto;
-  margin-top: 10rem;
-  margin-bottom: 5rem;
 
-  background: #dff6fc url(${logoImg}) no-repeat top;
-  box-shadow: 0 1px 4px 2px var(--black);
+  width: 100%;
+  height: 100vh;
 `;
 
 export const Content = styled.div`
-  form {
-    padding-top: 35%;
-    text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-    h1 {
-      margin: 0 0 30px 0;
-      font-weight: 300;
+  width: 85%;
+  max-width: 600px;
+
+  img {
+    width: 95%;
+    max-width: 350px;
+    margin: 20px 0 20px 0;
+  }
+
+  background: var(--light-blue);
+
+  border-radius: 7px;
+  box-shadow: 0 1px 15px 1px var(--black);
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    strong {
+      text-align: center;
+      font-size: 18px;
       color: var(--table-black);
+      margin: 15px;
+    }
+
+    div {
+      margin-top: 0;
+      width: 90%;
+
+      input {
+        width: 100%;
+        font-size: 14px;
+      }
     }
 
     a {
@@ -33,13 +58,40 @@ export const Content = styled.div`
       color: var(--table-black);
       transition: color 0.3s;
 
+      font-size: 14px;
+
       &:hover {
         color: var(--green);
       }
     }
 
     button {
-      margin-top: 20px;
+      width: 90%;
+      max-width: 300px;
+      margin: 20px 0 20px 0;
+      font-size: 14px;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    form {
+      strong {
+        font-size: 20px;
+      }
+
+      div {
+        input {
+          font-size: 16px;
+        }
+      }
+
+      a {
+        font-size: 16px;
+      }
+
+      button {
+        font-size: 16px;
+      }
     }
   }
 `;
