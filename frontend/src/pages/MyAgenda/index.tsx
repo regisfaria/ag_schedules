7848c630f6ecfaf181eb464179319bff23e3a35b
akioishiguro <akiosantos@gmail.com>
2020-08-20 16:37:37 -0300

    Get the api and verify, if that day, has any sched

--- frontend/src/pages/MyAgenda/index.tsx
+++ frontend/src/pages/MyAgenda/index.tsx
@@ -1,6 +1,15 @@
-import React, { useRef } from 'react';
+import React, {
+  useRef,
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+  ChangeEvent,
+} from 'react';
 import { Form } from '@unform/web';
+import { FormHandles, SubmitHandler } from '@unform/core';
 
+import { setDay } from 'date-fns/esm';
 import Radio from '../../components/RadioButton';
 import {
   Container,
@@ -8,22 +17,19 @@ import {
   WookSchedule,
   InicializePage,
   AfterChooseOneDay,
+  NewButton,
 } from './styles';
 
 import PageHeader from '../../components/PageHeader';
 import Menu from '../../components/Menu';
 import Main from '../../components/Main';
-import Section from '../../components/Section';
 import Select from '../../components/Select';
 import Button from '../../components/Button';
 
-import SelectHour from '../../components/SelectHours';
+import api from '../../services/api';
 
-interface CheckboxOption {
-  id: string;
-  value: string;
-  label: string;
-}
+import SelectHour from '../../components/SelectHours';
+import { useAuth } from '../../hooks/auth';
 
 interface FormData {
   consultState: string;
@@ -33,57 +39,107 @@ interface FormData {
   closeTImeMinute: string;
 }
 
+interface Days {
+  id: string;
+  day: number;
+  openTime: number;
+  closeTime: number;
+}
+
 export default function App() {
-  const formRef = useRef(null);
+  const formRef = useRef<FormHandles>(null);
+
+  const [workDays, setWorkDays] = useState<Days[]>([]);
+  const [choosenDay, setChoosenDay] = useState<Days>();
+  const [workToday, setWorkToday] = useState('');
 
   function handleSubmit(data: FormData) {
     console.log(data);
   }
 
-  const checkboxOptions: CheckboxOption[] = [
-    { id: 'sim', value: 'sim', label: 'Sim' },
-    { id: 'nao', value: 'nao', label: 'Não' },
-  ];
+  useEffect(() => {
+    api
+      .get<Days[]>('/schedules/9d2eb621-53f8-42cb-9177-5aabac578c6d')
+      .then(response => {
+        setWorkDays(response.data);
+      });
+  }, []);
+
+  // verify if the day is a work day or not
+  useEffect(() => {
+    if (!choosenDay) {
+      return;
+    }
+    choosenDay.openTime !== -1 ? setWorkToday('Sim') : setWorkToday('Não');
+  }, [choosenDay]);
+
+  //
+
+  const handleSelectedCity = useCallback(
+    (event: ChangeEvent<HTMLSelectElement>) => {
+      setWorkToday(event.target.value);
+    },
+    [],
+  );
+
+  useEffect(() => {
+    formRef.current?.setFieldValue('gender', workToday);
+  }, [workToday, choosenDay]);
+
+  console.log(choosenDay);
 
   return (
     <>
       <Menu />
 
-      <DaysWeek>
-        <Button>D</Button>
-
-        <Button>2°</Button>
-
-        <Button>3°</Button>
-
-        <Button>4°</Button>
-
-        <Button>5°</Button>
-
-        <Button>6°</Button>
-
-        <Button>S</Button>
-      </DaysWeek>
-
       <Container>
-        {/* <PageHeader
-        title="Meus Horarios"
-        subTitle="Marque abaixo os dias que você gostaria de trabalhar"
-      /> */}
+        <DaysWeek>
+          {workDays.map(dia => {
+            return (
+              <NewButton
+                key={dia.id}
+                onClick={() => {
+                  setChoosenDay(dia);
+                }}
+                workDay={dia.openTime === -1}
+                selectdAY={dia.id === choosenDay?.id}
+              >
+                {dia.day}
+              </NewButton>
+            );
+          })}
+        </DaysWeek>
 
-        <InicializePage>
-          <p>Clique em um dia da semana para começar a editar</p>
+        <InicializePage inicialize={workToday === ''}>
+          <div>
+            <p>Clique em um dia da semana para começar a editar</p>
+          </div>
         </InicializePage>
 
-        <AfterChooseOneDay>
+        <AfterChooseOneDay inicialize={workToday === ''}>
           <div>
             <strong>Realizar Consultas Nesse Dia?</strong>
+            <p>{workToday}</p>
           </div>
 
           <Form ref={formRef} onSubmit={handleSubmit}>
-            <Radio name="consultState" options={checkboxOptions} />
+            {/* <Radio
+              name="consultState"
+              options={[
+                { id: 'Sim', label: 'Sim' },
+                { id: 'Não', label: 'Não' },
+              ]}
+            /> */}
 
-            <WookSchedule>
+            <Select name="gender" onChange={handleSelectedCity}>
+              <option value={workToday} selected hidden>
+                {workToday}
+              </option>
+              <option value="Sim">Sim</option>
+              <option value="Não">Não</option>
+            </Select>
+
+            <WookSchedule work={workToday === 'Sim'}>
               <PageHeader
                 title="Horários de Trabalho"
                 subTitle="Marque abaixo os dias que você gostaria de trabalhar"
@@ -91,13 +147,7 @@ export default function App() {
               <SelectHour />
             </WookSchedule>
 
-            {/* <PageHeader
-          title="Hórarios de Descanso"
-          subTitle="Marque abaixo os dias que você gostaria de trabalhar"
-        />
-        <SelectHour /> */}
-
-            <Button type="submit">Done</Button>
+            <Button type="submit">Salvar</Button>
           </Form>
         </AfterChooseOneDay>
       </Container>
