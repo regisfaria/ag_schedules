6ed62470fb64bedc220cb21cf3f64fa3a4825838
akioishiguro <akiosantos@gmail.com>
2020-08-25 09:53:48 -0300

    formated hours

--- backend/src/services/Schedules/FormatSchedule.ts
+++ backend/src/services/Schedules/FormatSchedule.ts
@@ -6,33 +6,26 @@ class FormatSchedule {
     schedules.forEach(schedule => {
       switch (schedule.day) {
         case 0:
-          schedule.mobileFormatedDay = 'D';
-          schedule.desktopFormatedDay = 'Dom';
+          schedule.formatedDay = 'Dom';
           break;
         case 1:
-          schedule.mobileFormatedDay = '2ª';
-          schedule.desktopFormatedDay = 'Seg';
+          schedule.formatedDay = 'Seg';
           break;
         case 2:
-          schedule.mobileFormatedDay = '3ª';
-          schedule.desktopFormatedDay = 'Ter';
+          schedule.formatedDay = 'Ter';
           break;
 
         case 3:
-          schedule.mobileFormatedDay = '4ª';
-          schedule.desktopFormatedDay = 'Qua';
+          schedule.formatedDay = 'Qua';
           break;
         case 4:
-          schedule.mobileFormatedDay = '5ª';
-          schedule.desktopFormatedDay = 'Qui';
+          schedule.formatedDay = 'Qui';
           break;
         case 5:
-          schedule.mobileFormatedDay = '6ª';
-          schedule.desktopFormatedDay = 'Sex';
+          schedule.formatedDay = 'Sex';
           break;
         case 6:
-          schedule.mobileFormatedDay = 'S';
-          schedule.desktopFormatedDay = 'Sab';
+          schedule.formatedDay = 'Sab';
           break;
 
         default:
@@ -41,41 +34,45 @@ class FormatSchedule {
 
       if (schedule.openTime === -1 || schedule.closeTime === -1) {
         schedule.workDay = false;
-        schedule.formatedOpenTime = 'none';
+        schedule.formatedOpenHour = -1;
+        schedule.formatedOpenMinute = -1;
         delete schedule.openTime;
-        schedule.formatedCloseTime = 'none';
+        schedule.formatedCloseHour = -1;
+        schedule.formatedCloseMinute = -1;
         delete schedule.closeTime;
 
         return;
       }
 
       const openTimeMod = schedule.openTime % 60;
+
       if (openTimeMod === 0) {
         const parsedHour = schedule.openTime / 60;
-        const formatedHour = `${parsedHour}:00`;
 
-        schedule.formatedOpenTime = formatedHour;
+        schedule.formatedOpenHour = parsedHour;
+        schedule.formatedOpenMinute = 0;
         delete schedule.openTime;
       } else {
         const parsedHour = Math.floor(schedule.openTime / 60);
-        const formatedHour = `${parsedHour}:${openTimeMod}`;
 
-        schedule.formatedOpenTime = formatedHour;
+        schedule.formatedOpenHour = parsedHour;
+        schedule.formatedOpenMinute = openTimeMod;
         delete schedule.openTime;
       }
 
       const closeTimeMod = schedule.closeTime % 60;
+
       if (closeTimeMod === 0) {
         const parsedHour = schedule.closeTime / 60;
-        const formatedHour = `${parsedHour}:00`;
 
-        schedule.formatedCloseTime = formatedHour;
+        schedule.formatedCloseHour = parsedHour;
+        schedule.formatedCloseMinute = 0;
         delete schedule.closeTime;
       } else {
         const parsedHour = Math.floor(schedule.closeTime / 60);
-        const formatedHour = `${parsedHour}:${closeTimeMod}`;
 
-        schedule.formatedCloseTime = formatedHour;
+        schedule.formatedCloseHour = parsedHour;
+        schedule.formatedCloseMinute = closeTimeMod;
         delete schedule.closeTime;
       }
 
