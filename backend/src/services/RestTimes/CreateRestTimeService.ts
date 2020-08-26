ef6fceeac880a1afe14d03fb349da171eacc1fc2
akioishiguro <akiosantos@gmail.com>
2020-08-26 18:02:58 -0300

    Create Array for list rest schedules

--- backend/src/services/RestTimes/CreateRestTimeService.ts
+++ backend/src/services/RestTimes/CreateRestTimeService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 
 import RestTime from '../../models/RestTime';
 import ScheduleAvailability from '../../models/ScheduleAvailability';
+import AppError from '../../errors/AppError';
 
 import ConvertStringHourToInt from '../../utils/ConvertStringHourToInt';
 
@@ -26,6 +27,33 @@ class CreateRestTimeService {
     const scheduleAvailability = await schedulesRepository.findOne(
       scheduleAvailabilityId,
     );
+
+    const restStartAvailability = await restTimesRepository.find({
+      where: { startTime: parsedStartTime },
+    });
+
+    const restEndAvailability = await restTimesRepository.find({
+      where: { endTime: parsedEndTime },
+    });
+
+    if (restStartAvailability || restEndAvailability) {
+      throw new AppError(
+        'Você não pode marcar um intervalo em horarios já marcados ',
+      );
+    }
+
+    if (!scheduleAvailability?.openTime || !scheduleAvailability?.closeTime) {
+      throw new AppError('Horarios invalidos');
+    }
+
+    if (
+      parsedStartTime < scheduleAvailability.openTime ||
+      parsedEndTime > scheduleAvailability?.closeTime
+    ) {
+      throw new AppError(
+        'Você não pode marcar um intervalo fora do horario de trabalho',
+      );
+    }
 
     const restTime = restTimesRepository.create({
       scheduleAvailability,
---
+++ backend/src/services/RestTimes/ListSchedulesAvailableForRest.ts
@@ -0,0 +1,69 @@
+import { getRepository } from 'typeorm';
+
+import RestTime from '../../models/RestTime';
+import FormatRestTimes from './FormatRestTimesService';
+
+interface Request {
+  id: string;
+  openTime: number;
+  closeTime: number;
+}
+
+class ListSchedulesAvailableForRest {
+  public async execute({
+    id,
+    openTime,
+    closeTime,
+  }: Request): Promise<number[]> {
+    let formatedOpenHour = 0;
+    let formatedCloseHour = 0;
+    const restTimesRepository = getRepository(RestTime);
+
+    const unparsedRestTimes = await restTimesRepository.find({
+      where: { scheduleAvailabilityId: id },
+    });
+
+    const formatRestTimes = new FormatRestTimes();
+
+    const restTimes = formatRestTimes.execute(unparsedRestTimes);
+
+    const startTimeMod = openTime % 60;
+
+    if (startTimeMod === 0) {
+      formatedOpenHour = openTime / 60;
+    } else {
+      formatedOpenHour = Math.floor(openTime / 60);
+    }
+
+    const closeTimeMod = closeTime % 60;
+
+    if (closeTimeMod === 0) {
+      formatedCloseHour = closeTime / 60;
+    } else {
+      formatedCloseHour = Math.floor(closeTime / 60);
+    }
+
+    const createArrayForStartHours = Array.from(
+      { length: formatedCloseHour - formatedOpenHour - 1 },
+      (_, index) => index + formatedOpenHour + 1,
+    );
+
+    restTimes.forEach(element => {
+      const inicialValueRest = createArrayForStartHours.findIndex(
+        number => number === element.formatedStartHour,
+      );
+
+      const endValueRest = createArrayForStartHours.findIndex(
+        number => number === element.formatedEndHour,
+      );
+
+      const numberOfIndexForRemove = endValueRest - inicialValueRest;
+
+      createArrayForStartHours.splice(inicialValueRest, numberOfIndexForRemove);
+    });
+
+    return createArrayForStartHours;
+  }
+}
+
+export default ListSchedulesAvailableForRest;
