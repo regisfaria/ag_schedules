67fd43151b9b6e42ef5e426f4ae248549103d203
akioishiguro <akiosantos@gmail.com>
2020-09-03 11:56:38 -0300

    New function in hook reset

--- frontend/src/hooks/reset.tsx
+++ frontend/src/hooks/reset.tsx
@@ -3,20 +3,32 @@ import React, { createContext, useContext, useCallback, useState } from 'react';
 interface ResetContextData {
   resetCreateConsult: boolean;
   resetCreateConsultPage(renderState: boolean): void;
+  resetCreateRestTime: boolean;
+  resetCreateRestTimePage(renderState: boolean): void;
 }
 
 const ResetContext = createContext<ResetContextData>({} as ResetContextData);
 
 const ResetProvider: React.FC = ({ children }) => {
   const [resetCreateConsult, setResetCreateConsult] = useState(false);
+  const [resetCreateRestTime, setResetCreateRestTime] = useState(false);
 
   const resetCreateConsultPage = useCallback((renderState: boolean) => {
     setResetCreateConsult(renderState);
   }, []);
 
+  const resetCreateRestTimePage = useCallback((renderState: boolean) => {
+    setResetCreateRestTime(renderState);
+  }, []);
+
   return (
     <ResetContext.Provider
-      value={{ resetCreateConsult, resetCreateConsultPage }}
+      value={{
+        resetCreateConsult,
+        resetCreateConsultPage,
+        resetCreateRestTime,
+        resetCreateRestTimePage,
+      }}
     >
       {children}
     </ResetContext.Provider>
